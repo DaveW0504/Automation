bc73cdc152be1f471ec5a5d9ec2b822b0f9ec532
David Windle <134392649+DaveW0504@users.noreply.github.com>
2025-06-25 10:44:07 +0100

    Update main.tf

--- Terraform/TF-EC2Creation/main.tf
+++ Terraform/TF-EC2Creation/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "myserver" {
 	ami = var.ami_app
 	instance_type = "t2.micro"
 	key_name = var.ssh_key
-	subnet_id = "subnet-0d4931b99a4b67a61"
-  vpc_security_group_ids = ["sg-0fd1d7ca7724af63f"]
+	subnet_id = "subnet-[ENTER SUBNET ID]"
+  vpc_security_group_ids = ["sg-[ENTER SECURITY GROUP ID]"]
   associate_public_ip_address = true
 }
